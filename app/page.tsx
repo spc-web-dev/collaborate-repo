0ae1178fcb9038d94b045887c85216579f0b843f
spc-web-dev <itspc2023@gmail.com>
2025-01-06 16:32:38 +0700

    modify home page added header and paragrap

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
 
 export default function Home() {
   return (
     <div>
-      homepage 
+      <h1>Homepage</h1>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, voluptatum, nesciunt officiis a in optio reiciendis tempore itaque iusto corrupti voluptas odio aperiam, consectetur natus recusandae voluptate aspernatur dolorem necessitatibus?</p>
     </div>
   );
 }
