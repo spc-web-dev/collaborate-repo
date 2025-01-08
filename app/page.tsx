2ca80772db8e599f5e1200b751bde53f88b1b9b3
spc-web-dev <itspc2023@gmail.com>
2025-01-08 15:23:11 +0700

    install shandcn ui, dark theme and add button

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,18 @@
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <div>
-      <h1>Homepage</h1>
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae, voluptatum, nesciunt officiis a in optio reiciendis tempore itaque iusto corrupti voluptas odio aperiam, consectetur natus recusandae voluptate aspernatur dolorem necessitatibus?</p>
+    <div className="grid place-items-center h-screen w-full">
+      <div className="w-96 space-y-4">
+        <h1>Homepage</h1>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Repudiandae,
+          voluptatum, nesciunt officiis a in optio reiciendis tempore itaque
+          iusto corrupti voluptas odio aperiam, consectetur natus recusandae
+          voluptate aspernatur dolorem necessitatibus?
+        </p>
+        <Button variant={"outline"}>Home page button</Button>
+      </div>
     </div>
   );
 }
